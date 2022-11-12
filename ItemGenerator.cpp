d2232c8fc4b8ab6199dba5100cce4c81213b01e9
KevinTheGreat <Westminsterkevindo@gmail.com>
2022-11-11 20:46:02 -0800

    ItemGenerator now has constructor, getinstance and generate item function

--- ItemGenerator.cpp
+++ ItemGenerator.cpp
@@ -1,5 +1,40 @@
 #include "ItemGenerator.h"
+#include "Item.h"
+#include <fstream>
+#include <random>
+#include <iostream>
 
 ItemGenerator::ItemGenerator()
 {
+	std::string itemName;
+	std::ifstream file("itemList.txt");
+	if (file.is_open())
+	{
+		while (std::getline(file, itemName))
+		{
+			itemList.push_back(Item(itemName));
+		}
+	}
+	else
+	{
+		std::cout << "Unable to open item file";
+	}
+}
+
+ItemGenerator ItemGenerator::GetInstance()
+{
+	if (itemgenerator == nullptr)
+	{
+		itemgenerator = new ItemGenerator();
+	}
+	return *itemgenerator;
+}
+
+Item ItemGenerator::GenerateItem()
+{
+	std::random_device rd; // Obtain a random number from hardware
+	std::mt19937 gen(rd()); // Seed the generator
+	std::uniform_int_distribution<> distr(0,itemList.size()); // Define the range
+
+	int randNum = distr(gen);
 }
--- ItemGenerator.h
+++ ItemGenerator.h
@@ -1,9 +1,15 @@
 #pragma once
+#include "Item.h"
+#include <vector>
 class ItemGenerator
 {
 private:
-
+	static ItemGenerator* itemgenerator;
+	std::vector<Item> itemList;
 public:
 	ItemGenerator();
+	static ItemGenerator GetInstance();
+	Item GenerateItem();
+	~ItemGenerator();
 };
 
